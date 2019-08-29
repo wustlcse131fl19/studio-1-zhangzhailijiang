7ae2b66c9b1951fc1e5827f8467a8f228deb1dd0
zhanglunalnz <luna.zhang@wustl.edu>
2019-08-30 04:37:26 +0800

    STUDIO1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -9,6 +9,7 @@ public class Ordered {
 	 */
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
+		
 		int x = ap.nextInt("Value for x?");
 		int y = ap.nextInt("Value for y?");
 		int z = ap.nextInt("Value for z?");
